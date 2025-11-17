adb194face3a4e808647389bd323f8c5f57d2df3
Chaymae <dahhassichaymae@gmail.com>
2025-11-17 15:11:48 +0100

    Implement DocumentSplitter class for chunking Markdown documents

--- src/Splitter.py
+++ src/Splitter.py
@@ -1,1 +1,14 @@
 #Q1:chunking Markdown
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+class DocumentSplitter:
+    def _init_(self, chunk_size: int = 500, chunk_overlap: int = 50):
+        self.chunk_size = chunk_size
+        self.chunk_overlap = chunk_overlap
+
+    def split(self, documents):
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size,
+            chunk_overlap=self.chunk_overlap
+        )
+        return splitter.split_documents(documents)
