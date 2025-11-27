3ca719a6c8f54030ec8502a6e7d2175c3dc06d56
ELABDI-Ibrahim <133694690+ELABDI-Ibrahim@users.noreply.github.com>
2025-11-27 20:29:55 +0100

    Update splitter.py

--- src/splitter.py
+++ src/splitter.py
@@ -1,14 +1,1 @@
-#Q1:chunking Markdown
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-class DocumentSplitter:
-    def __init__(self, chunk_size: int = 500, chunk_overlap: int = 50):
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-
-    def split(self, documents):
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=self.chunk_size,
-            chunk_overlap=self.chunk_overlap
-        )
-        return splitter.split_documents(documents)
